4465d5de611db0f4fe56d5b8e32839773e1bd822
Grégoire Détrez <gregoire.detrez@gu.se>
2014-10-21 17:17:15 +0200

    Returns Lazy ByteStrings

--- Codec/Archive/Ar.hs
+++ Codec/Archive/Ar.hs
@@ -1,17 +1,18 @@
 module Codec.Archive.Ar where
 
 import Data.Binary.Get
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
-import Data.ByteString.Char8 (pack, unpack)
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.ByteString.Char8 (pack)
 import Data.Char (isSpace)
 import Control.Monad (when, liftM, unless)
 import Control.Applicative ((<$>), (<*>))
 -- import Control.Exception (assert)
 
-type Entry = (String, Int, Int, Int, Int, B.ByteString)
+type Entry = (String, Int, Int, Int, Int, ByteString)
 
-readAr :: L.ByteString -> [Entry]
+readAr :: ByteString -> [Entry]
 readAr = runGet $ do
     magicAsciiStr "!<arch>\LF"
     getEntries
@@ -27,7 +28,7 @@ getEntries = do
 
 -- | Read an int as a string of the given length
 getAsciiInt :: Int -> Get Int
-getAsciiInt l = liftM (read . unpack) (getByteString l)
+getAsciiInt l = liftM (read . unpack) (getLazyByteString (fromIntegral l))
 
 getEntry :: Get Entry
 getEntry = do
@@ -43,27 +44,27 @@ getEntry = do
     return (name, time, owner, group, mode, content)
 
 getArName :: Get String
-getArName = liftM (strip . unpack) (getByteString 16)
+getArName = liftM (strip . unpack) (getLazyByteString 16)
   where
     strip = dropWhile isSpace . reverse . dropSlash . dropWhile isSpace . reverse
     dropSlash ('/':s) = s
     dropSlash s = s
 
-getContent :: Get B.ByteString
+getContent :: Get ByteString
 getContent = do
     len <- getAsciiInt 10
-    fm <- getByteString 2
+    fm <- getLazyByteString 2
     unless (fm == fileMagic) (fail ("Invalid file magic " ++ unpack fm))
-    getByteString len
+    getLazyByteString (fromIntegral len)
   where
-    fileMagic = B.pack [0x60, 0x0A]
+    fileMagic = L.pack [0x60, 0x0A]
 
 
 -- | Expect to be able to read the given ASCII string.
 -- If it fails, no input is consumed.
 magicAsciiStr :: String -> Get ()
 magicAsciiStr s = do
-    bs <- lookAhead $ getByteString n
+    bs <- lookAhead $ getByteString (fromIntegral n)
     when (bs /= pack s) $ fail "Invalid magic ASCII string"
     skip n
   where
